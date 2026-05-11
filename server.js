1e6d7b0c559e783e5df1511341e81287a7233983
TokarIvan141 <tokar.ivan.a@chnu.edu.ua>
2026-05-11 19:23:42 +0300

    feat: implement categories CRUD, error handling and Swagger docs

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = require('./src/app.js');
 const connectMongo = require('./src/shared/database/mongo');
 const prisma = require('./src/shared/database/prisma');
 
+
 const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
@@ -10,14 +11,14 @@ const startServer = async () => {
         await connectMongo();
 
         await prisma.$connect();
-        console.log('🐘 PostgreSQL (Prisma) Connected');
+        console.log('PostgreSQL (Prisma) Connected');
 
         app.listen(PORT, () => {
             console.log(`Server is running on http://localhost:${PORT}`);
         });
 
     } catch (err) {
-        console.error('❌ Failed to start server:', err);
+        console.error('Failed to start server:', err);
         process.exit(1);
     }
 };
---
+++ src/modules/categories/category.repository.js
@@ -0,0 +1,67 @@
+const prisma = require('../../shared/database/prisma');
+
+class CategoryRepository {
+    async GetAll(userId, skip, take, search) {
+        const where = {
+            userId: userId,
+            deletedAt: null
+        };
+
+        if (search) {
+            where.name = {
+                contains: search,
+                mode: 'insensitive'
+            };
+        }
+
+        return await prisma.category.findMany({
+            where: where,
+            skip: skip,
+            take: take,
+            orderBy: { createdAt: 'desc' }
+        });
+    }
+
+    async CountAll(userId, search) {
+        const where = {
+            userId: userId,
+            deletedAt: null
+        };
+
+        if (search) {
+            where.name = {
+                contains: search,
+                mode: 'insensitive'
+            };
+        }
+
+        return await prisma.category.count({ where: where });
+    }
+
+    async GetById(id, userId) {
+        return await prisma.category.findFirst({
+            where: { id: id, userId: userId, deletedAt: null },
+            include: { transactions: true }
+        });
+    }
+
+    async Create(data) {
+        return await prisma.category.create({ data });
+    }
+
+    async Update(id, data) {
+        return await prisma.category.update({
+            where: { id: id },
+            data: data
+        });
+    }
+
+    async SoftDelete(id) {
+        return await prisma.category.update({
+            where: { id: id },
+            data: { deletedAt: new Date() }
+        });
+    }
+}
+
+module.exports = new CategoryRepository();
